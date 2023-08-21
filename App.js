5a9d54c8c005c980dd26e1fadca69e7842c15ca0
Vhora Abdulrauf <raufvhora4480@gmail.com>
2023-08-21 22:38:57 +0530

    dot env package added

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {useState, useCallback, useEffect} from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import {GiftedChat} from 'react-native-gifted-chat';
-
+import {API_URL, API_TOKEN} from '@env';
 const App = () => {
   const user = {
     _id: 1,
@@ -26,9 +26,43 @@ const App = () => {
         style={{
           alignItems: 'center',
           justifyContent: 'center',
-          backgroundColor: 'red',
+          flex: 1,
         }}>
-        <Text>Abdul</Text>
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: '700',
+          }}>
+          APK GENERATE FROM GITHUB ACTION
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: '700',
+            marginVertical: 10,
+            textDecorationLine: 'underline',
+          }}>
+          VALUE FROM .ENV FILE
+        </Text>
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: '500',
+            color: 'red',
+            marginVertical: 10,
+          }}>
+          API_URL ::{API_URL}
+        </Text>
+
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: '500',
+            color: 'red',
+            marginVertical: 10,
+          }}>
+          API_TOKEN ::{API_TOKEN}
+        </Text>
         <GiftedChat
           messages={messages}
           user={user}
